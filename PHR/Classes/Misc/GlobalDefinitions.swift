b6dfe5115ebd943661a8bdb052f3deac2fa5b5dd
akonst17 <akonst17@gmail.com>
2022-09-20 18:53:38 +0300

    интеграция sdk 2.0; новые урлы; build 8

--- PHR/Classes/Misc/GlobalDefinitions.swift
+++ PHR/Classes/Misc/GlobalDefinitions.swift
@@ -6,21 +6,24 @@
 //
 
 final class GlobalDefinitions {
-    static let domainUrl = "https://exams.korrekted.com"
+    static let domainUrl = "https://exams3.mastrapi.com"
     static let apiKey = "hV3KVwFeJjQd7qHy"
-
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "Lq5ZafHQNsSc9nvl"
 
     static let applicationTag = "EXAM_PHR"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "Lq5ZafHQNsSc9nvl"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "QknsokEJ9VXmSLm0"
 
     static let appStoreUrl = "https://apps.apple.com/app/phr-human-resources-prep/id1580533190"
     static let appleAppID = "1580533190"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
